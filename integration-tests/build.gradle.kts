8cead3fa9a0f5db10319309707a024455a783615
Qi Gao <qi.gao2012@gmail.com>
2026-08-21 19:12:16 +0800

    test: wire integration tests to published plugin repository

--- integration-tests/build.gradle.kts
+++ integration-tests/build.gradle.kts
@@ -6,8 +6,15 @@ dependencies {
     testImplementation(localGroovy())
     testImplementation(gradleTestKit())
     testImplementation(libs.junit.jupiter)
+    testRuntimeOnly(libs.junit.platform.launcher)
 }
 
 tasks.test {
     useJUnitPlatform()
+    dependsOn(rootProject.tasks.named("publishToTestPluginRepository"))
+    systemProperty(
+        "simpledsl.test.repo",
+        rootProject.layout.buildDirectory.dir("test-plugin-repo").get().asFile.absolutePath
+    )
+    systemProperty("simpledsl.test.version", rootProject.version.toString())
 }
